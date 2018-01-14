23325d34d23e50b3dbed1761656227d1d17800a0
Roberto Ribeiro <robertov82008@gmail.com>
2018-01-14 17:02:44 -0200

    class 5.2

--- app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/example/financask/ui/activity/ListaTransacoesActivity.kt
@@ -2,16 +2,12 @@ package com.example.financask.ui.activity
 
 import android.app.AlertDialog
 import android.app.DatePickerDialog
-import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import android.widget.DatePicker
-import android.widget.Toast
 import com.example.financask.R
 import com.example.financask.extension.formataParaBrasileiro
 import com.example.financask.model.Tipo
@@ -26,14 +22,14 @@ import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
 
+    private val transacoes: MutableList<Transacao> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes: List<Transacao> = transacoesDeExemplo()
-
-        configuraResumo(transacoes)
-        configuraLista(transacoes)
+        configuraResumo()
+        configuraLista()
 
         lista_transacoes_adiciona_receita.setOnClickListener {
             val  view: View = window.decorView
@@ -84,36 +80,30 @@ class ListaTransacoesActivity : AppCompatActivity() {
 
                         val transacaoCriada = Transacao(BigDecimal(valorDialog), categoriaDialog, Tipo.RECEITA, dataCalendar)
 
-                        Toast.makeText(this,
-                                "${transacaoCriada.valor} - " +
-                                "${transacaoCriada.categoria} - " +
-                                "${transacaoCriada.data.formataParaBrasileiro()} - " +
-                                "${transacaoCriada.tipo}",
-                                Toast.LENGTH_LONG).show()
+                        atualizaTransacoes(transacaoCriada)
+                        lista_transacoes_adiciona_menu.close(true)
                     })
                     .setNegativeButton("Cancelar", null)
                     .show()
         }
     }
 
-    private fun configuraResumo(transacoes: List<Transacao>) {
+    private fun atualizaTransacoes(transacao: Transacao) {
+        transacoes.add(transacao)
+        configuraLista()
+        configuraResumo()
+    }
+
+    private fun configuraResumo() {
         val view = window.decorView
         val resumoView = ResumoView(this, view, transacoes)
 
         resumoView.atualiza()
     }
 
-    private fun configuraLista(transacoes: List<Transacao>) {
+    private fun configuraLista() {
         lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
     }
 
-    private fun transacoesDeExemplo(): List<Transacao> {
-        return listOf(
-                Transacao(BigDecimal(20.5), "Almoço de final de semana", Tipo.DESPESA),
-                Transacao(BigDecimal(20.5), "Comida", Tipo.DESPESA),
-                Transacao(BigDecimal(100), "Economia", Tipo.RECEITA),
-                Transacao(valor = BigDecimal(200.0), tipo = Tipo.DESPESA),
-                Transacao(BigDecimal(500.0), "Prêmio", Tipo.RECEITA)
-        )
-    }
+
 }
